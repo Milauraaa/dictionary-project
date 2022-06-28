224cbe7468645d5e1b3484071e6e8d67ccd0ae2b
Milauraaa <milaauraaa@gmail.com>
2022-06-28 22:13:42 +0100

    add api documentation link

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,13 +1,23 @@
 import React, { useState } from "react";
+import axios from "axios";
 
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data[0]);
+  }
+
   function search(event) {
     event.preventDefault();
+    let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+    console.log(url);
+
+    axios.get(url).then(handleResponse);
   }
+
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
   }
